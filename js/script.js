6534c8131cb214fb640cf570b175d91caea4b6fa
jpfingsten <jasmine.pfingsten@gmail.com>
2022-04-11 17:11:59 -0400

    Created random image on button click

--- js/script.js
+++ js/script.js
@@ -7,7 +7,26 @@ const getImage = async function () {
     const res = await fetch('https://picsum.photos/v2/list?limit=100');
     const images = await res.json();
 
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+// STEP 6 OF 7: REVEAL A RANDOM IMAGE
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex]; // picking random element in an array
+
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author; //unsure
+    img.src = imageAddress; //unsure of syntax
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function () {
+    getImage();
+})
